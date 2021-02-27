2179c946af69da39cca8379bf00057de0f020199
aaron_tetracube <aaronpsam@gmail.com>
2021-02-27 20:21:05 +0530

    admin details added in table.

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,17 +1,46 @@
 <template>
-<div>
-    <router-link class="btn btn-outline-primary my-2 my-sm-0" to="/login">logout</router-link> 
     <div>
-        <h1>Welcome {{this.$route.params.curruser}}</h1>
-    </div>
-    <div  v-if="username=='Admin'">
-    <div v-for="user in users" v-bind:key="user">
-        <p>{{user.name}}</p>
-     </div>
-    </div>
-</div>
-
-  
+        <div>
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                        <a class="navbar-brand" href="/">LOGDATA</a>
+                        
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <router-link class="btn btn-outline-primary my-2 my-sm-0" to="/login">logout</router-link>
+                        </form>
+                        </div>
+                </div>
+            </nav>
+        </div>
+        <div>
+            <h1>Welcome {{this.$route.params.curruser}}</h1>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-md-3"></div>
+            <div class="col-md-9">
+                <table class="table table-bordered" v-if="username=='Admin'">
+                    <tr>
+                        <th>Name</th>
+                        <th>Username</th>
+                    </tr>
+                    <tr v-for="user in users" v-bind:key="user">
+                        <td>{{user.name}}</td>
+                        <td>{{user.username}}</td>
+                    </tr>
+                </table>
+            </div>
+            <div class="col-md-3"></div>
+            
+        </div>
+        
+               
+    </div> 
 </template>
 
 <script>
@@ -36,4 +65,15 @@ methods:{
 }
 }
 </script>
+<style scoped>
+table{
+    justify-content: center;
+    border: black solid;
+    background-color: aquamarine;
+    width: 50%;
+}
+.col-md-9{
+    padding-left: 80px;
+}
+</style>
  
