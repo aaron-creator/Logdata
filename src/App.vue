590f2bd0941753dfb89787af5b22fd5b2e5884f7
aaron_tetracube <aaronpsam@gmail.com>
2021-02-27 13:06:56 +0530

    login updated and user register completed.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,6 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div> -->
-    <br />
     <router-view />
   </div>
 </template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
+  <div class="container-fluid">
+    <Navbar />
     <h1>This is an about page</h1>
+     <h1>This is an about page</h1>
+      <h1>This is an about page</h1>
   </div>
 </template>
+
+<script>
+import Navbar from "../components/Navbar.vue"
+export default {
+  components: {
+    Navbar
+    }
+}
+</script>
+<style scoped>
+
+</style>
